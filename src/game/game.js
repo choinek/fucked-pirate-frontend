17607fe8e0cabbf29356b34744ebfbc0c9b45eef
Maciek Kostrzycki <m.kostrzycki@global4net.com>
2020-01-11 13:13:07 +0100

    Added cursors and player movement.

--- src/game/game.js
+++ src/game/game.js
@@ -14,14 +14,18 @@ const config = {
             gravity: {
                 y: 800
             },
-            debug: false
+            debug: true
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
+
+let player;
+let cursors;
 
 export class Game extends Phaser.Game {
 
@@ -35,7 +39,35 @@ function preload()
 
 function create()
 {
-    this.add.image(32, 59, 'pirate-johntardo');
+    player = this.physics.add.image(32, 59, 'pirate-johntardo');
+
+    player.setBounce(0.2);
+    player.body.setGravityY(300);
+    player.setCollideWorldBounds(true);
+}
+
+function update ()
+{
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+    }
+    else
+    {
+        player.setVelocityX(0);
+    }
+
+    // if (cursors.up.isDown && player.body.touching.down)
+    if (cursors.up.isDown)
+    {
+        player.setVelocityY(-330);
+    }
 }
 
 export function createGame() {
