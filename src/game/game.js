8cd993e7c31a490fe2f11cf769cd07985852d39b
Maciek Kostrzycki <m.kostrzycki@global4net.com>
2020-01-11 20:52:49 +0100

    Added tile set and tile map.

--- src/game/game.js
+++ src/game/game.js
@@ -27,6 +27,37 @@ const config = {
 let player;
 let cursors;
 
+const levelJson = {
+    assets: {
+        tileset: {
+            key: 'tiles',
+            path: 'assets/tilesets/deep-forest-tileset-32.png'
+        },
+        tilemap: {
+            json: {
+                key: 'map',
+                path: 'assets/tilemaps/deep-forest.json'
+            },
+            layers: [
+                {
+                    name: 'deepBackgroundLayer',
+                    layerName: 'Deep Background',
+                    tileSet: 'tileset',
+                    x: 0,
+                    y: 0
+                }
+            ]
+        },
+        images: [
+            {
+                key: 'pirate-johntardo',
+                path: 'assets/pirate-johntardo.png',
+            }
+        ]
+    },
+
+};
+
 export class Game extends Phaser.Game {
 
 
@@ -35,15 +66,30 @@ export class Game extends Phaser.Game {
 function preload()
 {
     this.load.image('pirate-johntardo', 'assets/pirate-johntardo.png');
+
+    this.load.image('tiles', 'assets/tilesets/deep-forest-tileset-32.png');
+    this.load.tilemapTiledJSON('map', 'assets/tilemaps/deep-forest.json');
 }
 
 function create()
 {
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('DeepForestTileset32', 'tiles');
+
+    const deepBackgroundLayer = map.createStaticLayer("Deep Background", tileset, 0, 0);
+    const treesLayer = map.createStaticLayer("Trees", tileset, 0, 0);
+    const backgroundLayer = map.createStaticLayer("Background", tileset, 0, 0);
+    const objectsLayer = map.createStaticLayer("Objects", tileset, 0, 0);
+
+    objectsLayer.setCollisionByProperty({ Collide: true });
+
     player = this.physics.add.image(32, 59, 'pirate-johntardo');
 
     player.setBounce(0.2);
     player.body.setGravityY(300);
     player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, objectsLayer);
 }
 
 function update ()
